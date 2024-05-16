7c7af9471ca9dfa1d1cec3a0c80792aabea9a4d4
Maryogi123 <167964284+Maryogi123@users.noreply.github.com>
2024-05-16 17:52:19 +0700

    Add files via upload

---
+++ app/Http/Models/Pegawai.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pegawai extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_pegawai';
+    protected $table = 'pegawai';
+    protected $fillable = [
+        'nama_pegawai',
+        'alamat',
+        'jenis_kelamin',
+        'jabatan',
+        'status',
+    ];
+
+    public $timestamp = false;
+}
